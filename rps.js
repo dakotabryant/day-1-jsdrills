e001b4aa83b75d9e6f48d91e14f9bd12b5955fd6
Dakota Bryant <bryant.dakota@gmail.com>
2017-04-03 15:42:14 -0500

    fixed the if/else statements

--- rps.js
+++ rps.js
@@ -2,40 +2,52 @@ function RockPaperScissors(userChoice){
     //userChoice = prompt('Rock, Paper or Scissors?').parseInt();
     let computer = 1 + Math.floor((Math.random()*3));
     let result;
-    switch(userChoice) {
-        case 1:
-            if (computer === 2) {
-                result = 'lost';
-            } else if (computer === 3) {
-                result = 'win';
-            } else {
-              result = 'tie';
-            }
-            break;
-        case 2:
-            if (computer === 3) {
-                result = 'lost';
-            } else if (computer === 1) {
-                result = 'win';
-            } else {
-              result = 'tie';
-            }
-            break;
-        case 3:
-            if (computer === 1) {
-                result = 'lost';
-            } else if (computer === 2) {
-                result = 'win';
-            } else {
-              result = 'tie';
-            }
-            break;
-        default:
-            console.log('Go back and pick a number between 1 and 3');
+
+    if(userChoice === computer) {
+      result = 'tie';
+    } else if(userChoice === 1 && computer === 2) {
+      result = 'lost';
+    } else if(userChoice === 2 && computer === 3) {
+      result = 'lost';
+    } else if(userChoice === 3 && computer === 1) {
+      result = 'lost';
+    } else {
+      result = 'won';
     }
-    return `You picked ${userChoice} and computer chose ${computer}. You ${result}.`;
-}
+    return `You picked ${userChoice} and the computer picked ${computer}. You ${result}.`
+  }
+
 
 //1 = rock ** 2 = paper ** 3 = scissor
 var result = RockPaperScissors(2);
 document.write(result);
+
+
+
+
+
+//     switch(userChoice) {
+//
+//         case 1:
+//             if (computer === 2) {
+//                 result = 'lost';
+//             } else if (computer === 3) {
+//                 result = 'win';
+//             }
+//         case 2:
+//             if (computer === 3) {
+//                 result = 'lost';
+//             } else if (computer === 1) {
+//                 result = 'win';
+//             }
+//         case 3:
+//             if (computer === 1) {
+//                 result = 'lost';
+//             } else if (computer === 2) {
+//                 result = 'win';
+//             }
+//         default:
+//             result = tie;
+//     }
+//     return `You picked ${userChoice} and computer chose ${computer}. You ${result}.`;
+// }
