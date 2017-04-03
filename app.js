afc5c83ddcb21ba0e2c67e1bbb464dbdbaeeeb98
Dakota Bryant <bryant.dakota@gmail.com>
2017-04-03 11:15:42 -0500

    added ability to check validity of arguments

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-function yearOfBirth(age) {  
+function yearOfBirth(age) {
     const yob = 2016 - age;
-  try {    
+  try {
     if(age < 0) throw new Error(`Age cannot be negative.`);
   } catch(err) {
     console.log(err);
@@ -10,9 +10,16 @@ function yearOfBirth(age) {
 
 const yob = yearOfBirth(25);
 
-function whoAmI (name, age) {      
-  console.log(`Hi, my name is ${name} and I'm ${age} years old`);
-  console.log(`I was born in ${yob}`);
+function whoAmI (name, age) {
+  try {
+    if(name.length < 1 || typeof(age) !== 'number') throw new Error(`Arguments not valid`);
+    console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+    console.log(`I was born in ${yob}`);
+  }
+  catch(err){
+    console.log(err);
+  }
+
 };
 
-whoAmI('Dakota', 25);
+whoAmI('Dakota', 'Dakota');
