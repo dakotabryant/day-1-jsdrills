7b17bdc22df0033957dc57620f5386fa3117a8bb
BAAMOSK <bunth667@gmail.com>
2017-04-03 12:28:51 -0400

    checked for typeof string in name argument

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ function yearOfBirth(age) {
 const yob = yearOfBirth(25);
 
 function whoAmI (name, age) {
+    console.log(typeof name);
   try {
-    if(name.length < 1 || typeof(age) !== 'number') throw new Error(`Arguments not valid`);
+    if(typeof name !== 'string' || typeof(age) !== 'number') throw new Error(`Arguments not valid`);
     console.log(`Hi, my name is ${name} and I'm ${age} years old`);
     console.log(`I was born in ${yob}`);
   }
@@ -22,4 +23,10 @@ function whoAmI (name, age) {
 
 };
 
-whoAmI('Dakota', 'Dakota');
+whoAmI('Dakota', 24);
+
+
+// QUestions for Rich
+// 1. Talk about syntax on lines 17 and 18. Is this correct?
+// 2. How to have multiple conditionals for 1 argument
+// 3. DRY for yob. Be able to pull argument from whoAmI function and pass to yearOfBirth function
