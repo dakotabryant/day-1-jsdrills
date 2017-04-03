0f77064a3e398e21c537afa8067ce0c19cd6f421
Dakota Bryant <bryant.dakota@gmail.com>
2017-04-03 10:35:39 -0500

    added year of birth function

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-var whoAmI = function() {
-    console.log(`Hi, my name is Dakota and I'm 25 years old`);
+function yearOfBirth(age) {
+  const yob = 2016 - age;
+  return yob;
+}
+const yob = yearOfBirth(25);
+
+function whoAmI (name, age) {
+  console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+  console.log(`I was born in ${yob}`);
 };
 
-whoAmI();
+whoAmI('Dakota', 24);
