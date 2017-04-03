8a426c17447ed26fcbe046a622e0fcd48c3ff9c3
BAAMOSK <bunth667@gmail.com>
2017-04-03 12:02:36 -0400

    added try-catch block

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-function yearOfBirth(age) {
-  const yob = 2016 - age;
+function yearOfBirth(age) {  
+    const yob = 2016 - age;
+  try {    
+    if(age < 0) throw new Error(`Age cannot be negative.`);
+  } catch(err) {
+    console.log(err);
+  }
   return yob;
 }
+
 const yob = yearOfBirth(25);
 
-function whoAmI (name, age) {
+function whoAmI (name, age) {      
   console.log(`Hi, my name is ${name} and I'm ${age} years old`);
   console.log(`I was born in ${yob}`);
 };
 
-whoAmI('Dakota', 24);
+whoAmI('Dakota', 25);
