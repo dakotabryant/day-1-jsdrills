b98de81e3766f701c0ce1daf28b8c63aec5ca7dc
Dakota Bryant <bryant.dakota@gmail.com>
2017-04-03 13:00:26 -0500

    added final drill 1 code

--- app.js
+++ app.js
@@ -1,29 +1,20 @@
 function yearOfBirth(age) {
-    const yob = 2016 - age;
-  try {
-    if(age < 0) throw new Error(`Age cannot be negative.`);
-  } catch(err) {
-    console.log(err);
-  }
-  return yob;
+  if(age < 0) throw new Error(`Age cannot be negative.`);
+  return 2016 - age;
 }
 
-const yob = yearOfBirth(25);
-
 function whoAmI (name, age) {
-    console.log(typeof name);
-  try {
-    if(typeof name !== 'string' || typeof(age) !== 'number') throw new Error(`Arguments not valid`);
-    console.log(`Hi, my name is ${name} and I'm ${age} years old`);
-    console.log(`I was born in ${yob}`);
-  }
-  catch(err){
-    console.log(err);
-  }
-
+  if (typeof name !== 'string' || name.length < 1 || typeof(age) !== 'number') throw new Error(`Arguments not valid`);
+  console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+  console.log(`I was born in ${yearOfBirth(age)}`);
 };
 
-whoAmI('Dakota', 24);
+try {
+  whoAmI('Dakota', 25);
+}
+catch(err){
+  console.log(err);
+}
 
 
 // QUestions for Rich
